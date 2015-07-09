e6336a67aded5ece1a1b48d51f5787cdb7c14242
Steve Castle <steve.castle@drssmail.com>
2015-07-09 09:22:46 -0700

    moved settings file to deployment scripts

--- deployment/files/settings/dev_settings.py
+++ deployment/files/settings/dev_settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'reversion'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'easytrack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'drss_db',                      # Or path to database file if using sqlite3.
+        'USER': 'django',                      # Not used with sqlite3.
+        'PASSWORD': 'z3ld2',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
