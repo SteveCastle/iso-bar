14fa8dd237d1d166087033b7417f752f9f465652
Steve Castle <steve.castle@drssmail.com>
2015-07-10 12:20:23 -0700

    Core test

--- easytrack/core/tests.py
+++ easytrack/core/tests.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+
+class SampleTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_twenty_six(self):
+        """Test"""
+        self.assertEqual(26, 26)
