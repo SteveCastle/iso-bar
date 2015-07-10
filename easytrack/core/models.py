5af24addeebdb2c27f7e02c0792d12fd1a927a88
Steve Castle <steve.castle@drssmail.com>
2015-07-10 13:00:20 -0700

    fixed contact unicode

--- easytrack/core/models.py
+++ easytrack/core/models.py
@@ -6,4 +6,4 @@ class Contact(models.Model):
     last_name = models.CharField(max_length=200)
 
     def __unicode__(self):
-        return "$s $s" (self.first_name, self.last_name)
+        return "$s $s"%(self.first_name, self.last_name)
