6190d951ae4df1359f996af329f58675a3f313ed
Steve Castle <steve.castle@drssmail.com>
2015-07-10 13:17:19 -0700

    Added contact creation test

--- easytrack/core/tests.py
+++ easytrack/core/tests.py
@@ -4,7 +4,12 @@ class ContactTestCase(TestCase):
     def setUp(self):
         Contact.objects.create(first_name="Stephen", last_name = "Castle")
 
+    def test_object_creation(self):
+        """ Contact can be created """
+        contact = Contact(first_name="Marge", last_name="Simpson")
+        self.assertIsInstance(contact, Contact)
+
     def test_contact_unicode_is_fullname(self):
-        """Test that a contact is created"""
+        """Test that a contact unicode method returns a full name"""
         contact = Contact.objects.get(first_name="Stephen")
         self.assertEqual(contact.__unicode__(), "Stephen Castle")
