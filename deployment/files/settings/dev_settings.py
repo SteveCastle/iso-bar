bf7e4d0633cdc77b391a51dfbae3fb2c185ed5da
Steve Castle <steve.castle@drssmail.com>
2015-07-11 12:48:19 -0700

    changed db user to app name

--- deployment/files/settings/dev_settings.py
+++ deployment/files/settings/dev_settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'isobar_db',                      # Or path to database file if using sqlite3.
-        'USER': 'django',                      # Not used with sqlite3.
+        'USER': 'isobar',                      # Not used with sqlite3.
         'PASSWORD': 'secretpassword',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
