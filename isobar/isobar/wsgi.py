de0042ec31c5d9465559bf96aa24b6bc3f2bb614
Steve Castle <steve.castle@drssmail.com>
2015-07-11 12:40:20 -0700

    generalized project name

--- isobar/isobar/wsgi.py
+++ isobar/isobar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for easytrack project.
+WSGI config for isobar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "easytrack.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "isobar.settings")
 
 application = get_wsgi_application()
