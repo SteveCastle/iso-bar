d811c4cc831e628dadf79791009d5b0c104fc95a
Steve Castle <steve.castle@drssmail.com>
2015-07-10 19:25:18 -0700

    Updated dev password

--- deployment/files/settings/dev_settings.py
+++ deployment/files/settings/dev_settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'drss_db',                      # Or path to database file if using sqlite3.
         'USER': 'django',                      # Not used with sqlite3.
-        'PASSWORD': 'z3ld2',                  # Not used with sqlite3.
+        'PASSWORD': 'secretpassword',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
